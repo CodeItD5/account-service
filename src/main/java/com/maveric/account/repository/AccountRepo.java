bf29fe642bcaba03c09db50a8938e6dc67643bd7
= <=>
2022-07-16 19:47:34 +0530

    Account service created

--- src/main/java/com/maveric/account/repository/AccountRepo.java
+++ src/main/java/com/maveric/account/repository/AccountRepo.java
@@ -2,6 +2,9 @@ package com.maveric.account.repository;
 
 import com.maveric.account.model.Account;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface AccountRepo extends MongoRepository<Account, String> {
 }
