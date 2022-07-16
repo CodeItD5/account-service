ce214deb6e789ad0a32e26ec28bb584c42f252b4
= <=>
2022-07-16 19:32:35 +0530

    createAccountEndPoint updated

--- src/main/java/com/maveric/account/controller/AccountController.java
+++ src/main/java/com/maveric/account/controller/AccountController.java
@@ -1,18 +1,18 @@
 package com.maveric.account.controller;
 
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.maveric.account.model.Account;
+import com.maveric.account.model.AccountDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/customers/")
 public class AccountController {
 
     @PostMapping("{customerId}/accounts")
-    public void createAccount(@PathVariable String customerId){
-
+    public ResponseEntity<Account> createAccount(@PathVariable String customerId, @RequestBody AccountDTO accountDTO){
+        return new ResponseEntity<>(AccountService.createAccount(accountDTO));
     }
 
 }
